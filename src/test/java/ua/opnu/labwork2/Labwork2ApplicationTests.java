80265cd32962b233feebe2668184555e5b84050d
Agehesha <romashugaev99@gmail.com>
2026-06-14 12:51:46 +0300

    Create Labwork2ApplicationTests.java

---
+++ src/test/java/ua/opnu/labwork2/Labwork2ApplicationTests.java
@@ -0,0 +1,21 @@
+package ua.opnu.labwork2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(properties = {
+        "spring.datasource.url=jdbc:h2:mem:testdb;MODE=PostgreSQL;DATABASE_TO_LOWER=TRUE;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.jpa.hibernate.ddl-auto=create-drop",
+        "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+        "spring.jpa.show-sql=false"
+})
+class Labwork2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
